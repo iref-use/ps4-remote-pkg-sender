c93aa9241380769ca6dc4ed1b72b0b6cb823abd3
iref <iref@exmdf.com>
2018-11-07 14:32:16 +0300

    add application menu
    fix OSX bug to paste with Cmd key

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const {app, BrowserWindow} = require('electron');
+const Menu = require('electron').Menu
 let mainWindow;
 
 function createWindow () {
@@ -10,8 +11,66 @@ function createWindow () {
   })
 }
 
+function createMenu() {
+  const application = {
+    label: "Application",
+    submenu: [{
+      label: "Quit",
+      accelerator: "Command+Q",
+      click: () => {app.quit()}
+    }]
+  }
+
+  const edit = {
+    label: "Edit",
+    submenu: [
+      {
+        label: "Undo",
+        accelerator: "CmdOrCtrl+Z",
+        selector: "undo:"
+      },
+      {
+        label: "Redo",
+        accelerator: "Shift+CmdOrCtrl+Z",
+        selector: "redo:"
+      },
+      {
+        type: "separator"
+      },
+      {
+        label: "Cut",
+        accelerator: "CmdOrCtrl+X",
+        selector: "cut:"
+      },
+      {
+        label: "Copy",
+        accelerator: "CmdOrCtrl+C",
+        selector: "copy:"
+      },
+      {
+        label: "Paste",
+        accelerator: "CmdOrCtrl+V",
+        selector: "paste:"
+      },
+      {
+        label: "Select All",
+        accelerator: "CmdOrCtrl+A",
+        selector: "selectAll:"
+      }
+    ]
+  };
+
+  const template = [
+    application,
+    edit
+  ];
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+}
+
 app.on('ready', function () {
   createWindow();
+  createMenu();
 });
 
 app.on('window-all-closed', function () {
